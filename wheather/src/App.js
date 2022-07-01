5d76740e44d40315e5cdc8e57ac64149c77662a5
Paradoxumus <ser-egorka@yandex.ru>
2022-07-02 00:54:47 +0400

    Сделали запрос

--- wheather/src/App.js
+++ wheather/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
+const lat = '54.22698';
+const lon = '49.56846';
+
+
+const API_key = "688f3cfe6eb6211b3e466e410b5a1c4a";
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=ru`
 function App() {
+
+  const [cloudDescription, setcloudDescription] =  useState('пусто')
+
+  useEffect(() => {
+    const fetchData = async () => {
+    const recponse = await fetch(url)
+    if(recponse.ok){
+      const json = await recponse.json()
+      setcloudDescription(json.weather[0].description)
+    }
+    else{
+      console.error('Ошибка')
+    }
+  }
+  fetchData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cloudDescription}
     </div>
   );
 }
