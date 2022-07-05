b5a4c2a4e660d030ef2a6a636d659682db0ca6ef
Paradoxumus <ser-egorka@yandex.ru>
2022-07-05 11:53:39 +0400

    Добавил иконки

--- wheather/src/App.js
+++ wheather/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import SvgSelector from './SvgSelector';
+
+
 
 const lat = '54.22698';
 const lon = '49.56846';
@@ -11,13 +13,19 @@ const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lo
 function App() {
 
   const [cloudDescription, setcloudDescription] =  useState('пусто')
+  const [DailyTemp, setDailyTemp] =  useState('пусто')
+  const [City, setCity] =  useState('пусто')
+  const [cloudDescriptionIcon, setcloudDescriptionIcon] =  useState('02d')
 
   useEffect(() => {
     const fetchData = async () => {
     const recponse = await fetch(url)
     if(recponse.ok){
       const json = await recponse.json()
+      setCity(json.name)
       setcloudDescription(json.weather[0].description)
+      setDailyTemp(Math.round(json.main.temp)+'°')
+      setcloudDescriptionIcon(json.weather[0].icon)
     }
     else{
       console.error('Ошибка')
@@ -28,8 +36,22 @@ function App() {
 
   return (
     <div className="App">
-      {cloudDescription}
+      <div className='City'>
+        {City}
+      </div>
+      <div className="MainMidle">
+        <div className="cloudDescriptionIcon">
+        <SvgSelector cssClass="cloudDescriptionIcon" code = "11d"/>
+        </div>
+        <div className="MainTemp">
+        {DailyTemp}
+        </div>
+      </div>
+      <div className="cloudDescription">
+        {cloudDescription}
+      </div>
     </div>
+    
   );
 }
 
---
+++ wheather/src/SvgSelector.js
@@ -0,0 +1,86 @@
+import PropTypes from "prop-types";
+import Sun from "./images/icons/Sun.svg";
+import Cloud from "./images/icons/Cloud.svg";
+import ScattedCloud from "./images/icons/CloudScattered.svg";
+import BrokenCloud from "./images/icons/BrockenClouds.svg";
+import ShowerRain from "./images/icons/ShowerRain.svg";
+import Rain from "./images/icons/Rain.svg";
+import Thunderstorm from "./images/icons/Thunderstorm.svg";
+import Snow from "./images/icons/Snow.svg";
+import Mist from "./images/icons/Mist.svg";
+
+function SvgSelector(props) {
+    const { cssClass, code } = props;
+    let svgSelect;
+    switch(code){
+        case"01d":
+        svgSelect = Sun;
+        break;
+        case"01n":
+        svgSelect = Sun;
+        break;
+        case"02d":
+        svgSelect = Cloud;
+        break;
+        case"02n":
+        svgSelect = Cloud;
+        break;
+        case"03d":
+        svgSelect = ScattedCloud;
+        break;
+        case"03n":
+        svgSelect = ScattedCloud;
+        break;
+        case"04d":
+        svgSelect = BrokenCloud;
+        break;
+        case"04n":
+        svgSelect = BrokenCloud;
+        break;
+        case"09d":
+        svgSelect = ShowerRain;
+        break;
+        case"09n":
+        svgSelect = ShowerRain;
+        break;
+        case"10d":
+        svgSelect = Rain;
+        break;
+        case"10n":
+        svgSelect = Rain;
+        break;
+        case"11d":
+        svgSelect = Thunderstorm;
+        break;
+        case"11n":
+        svgSelect = Thunderstorm;
+        break;
+        case"13d":
+        svgSelect = Snow;
+        break;
+        case"13n":
+        svgSelect = Snow;
+        break;
+        case"50d":
+        svgSelect = Mist;
+        break;
+        case"50n":
+        svgSelect = Mist;
+        break;
+        default:
+        svgSelect = null;
+        break
+    }
+    return <img className={cssClass} src={svgSelect} alt={code}/>
+}
+SvgSelector.propTypes = {
+    code: PropTypes.string,
+    cssClass: PropTypes.string,
+  };
+  
+  SvgSelector.defaultProps = {
+    code: "02n",
+    cssClass: "cloudDescriptionIcon",
+  };
+  
+  export default SvgSelector;
